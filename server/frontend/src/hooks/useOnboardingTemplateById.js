790e6bc352b3fcd521eacdcf8e57ec476dd9a804
feedegr <feedecs@hotmail.com>
2026-05-19 15:09:02 -0300

    fix import path in useOnboardingTemplateById

--- server/frontend/src/hooks/useOnboardingTemplateById.js
+++ server/frontend/src/hooks/useOnboardingTemplateById.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { getOnboardingTemplateById } from '../../services/onboardingService';
+import { getOnboardingTemplateById } from '../services/onboardingService';
 
 const useOnboardingTemplateById = (templateId) => {
 
